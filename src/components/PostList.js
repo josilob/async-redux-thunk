6a675c063ce664341a66a673d0a015f4ce9ad7ba
josilob <bjosilo@gmail.com>
2021-02-23 20:03:34 -0500

    posts passed and rendered

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,8 +6,30 @@ class PostList extends React.Component {
 	componentDidMount() {
 		this.props.fetchPosts();
 	}
+
+	renderList() {
+		return this.props.posts.map((post) => {
+			return (
+				<div className='item' key={post.id}>
+					<i className='large middle aliogned icon user' />
+					<div className='content'>
+						<div className='desription'>
+							<h2>{post.title}</h2>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	}
+
 	render() {
-		return <div>Post List</div>;
+		return <div className='ui relaxed divided list'>{this.renderList()}</div>;
 	}
 }
-export default connect(null, { fetchPosts: fetchPosts })(PostList);
+
+const mapStateToProps = (state) => {
+	return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostList);
